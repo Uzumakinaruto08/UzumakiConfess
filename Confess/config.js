c7a97c5cfa81651c3bea09ed2c3606584ed5bf69
Uzumakinaruto08 <125802104+Uzumakinaruto08@users.noreply.github.com>
2023-09-20 23:38:05 +0800

    Add files via upload

---
+++ Confess/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "BABYCAKES",
+    introTitle: 'Confess your Crush',
+    introDesc: `mahalin mo lang ako ng konti, ako na ang bahalang magpadami!`,
+    btnIntro: '^^CLICK ME^^',
+    title: 'Love isn’t something you find. Love is something that finds you. 🥰',
+    desc: 'Pwede koba marinig ang matamis mong OO!! ',
+    btnYes: 'Yes 🥰',
+    btnNo: 'No 😭',
+    question: 'Araw Araw ko Hinihiling na Sana Maging Akin Ka At Iingatan Kita 🥰',
+    btnReply: 'CLICK ME PLSSS 😭',
+    reply: 'Hehehe, Ang Ganda mo Talaga Lalo na Ang Mga Ngiti mo ',
+    mess: 'Hehehe, Ang Ganda mo Talaga Lalo na Ang Mga Ngiti mo 😘😘',
+    messDesc: 'Ingat Ka Palagi, SMile Kalang Palagi Andito lang ako para sayo.',
+    btnAccept: 'MWUAAAAAA😘😘',
+    messLink: 'https://github.com/Uzumakinaruto08' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
